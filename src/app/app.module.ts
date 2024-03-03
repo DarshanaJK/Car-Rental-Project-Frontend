2a0b28a33ed00e3c504104785527eff3b44778a7
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2024-03-03 19:16:04 +0530

    Create Navbar and Signup Reactive Form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//NG ZORRO IMPORTS
+import {NzSpinModule} from "ng-zorro-antd/spin";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzLayoutModule} from "ng-zorro-antd/layout";
+import {NzButtonModule} from "ng-zorro-antd/button";
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+
+        //NG ZORRO IMPORTS
+        NzSpinModule,
+        NzFormModule,
+        NzInputModule,
+        NzLayoutModule,
+        NzButtonModule,
+        ReactiveFormsModule,
+    ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
